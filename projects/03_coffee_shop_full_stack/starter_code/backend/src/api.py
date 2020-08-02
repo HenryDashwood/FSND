c35a7b588ffe3e94a3137f54d5bf869aab27191c
Henry Dashwood <henry.dashwood@curationcorp.com>
2020-08-02 09:36:30 +0100

    fix domain prefix, and other style changes

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -3,6 +3,7 @@ from flask import Flask, request, jsonify, abort
 from sqlalchemy import exc
 import json
 from flask_cors import CORS
+import sys
 
 from .database.models import db_drop_and_create_all, setup_db, Drink
 from .auth.auth import AuthError, requires_auth
@@ -24,8 +25,9 @@ db_drop_and_create_all()
     GET /drinks
         it should be a public endpoint
         it should contain only the drink.short() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks 
-    is the list of drinks or appropriate status code indicating reason for failure
+    returns status code 200 and json {"success": True, "drinks": drinks} where
+    drinks is the list of drinks or appropriate status code indicating reason
+    for failure
 '''
 @app.route('/drinks', methods=['GET'])
 def drinks():
@@ -33,7 +35,8 @@ def drinks():
         drinks = Drink.query.all()
         drinks = [drink.short() for drink in drinks]
         return {'success': True, 'drinks': drinks}, 200
-    except:
+    except Exception as e:
+        print(sys.exc_info)
         abort(404)
 
 
@@ -42,8 +45,9 @@ def drinks():
     GET /drinks-detail
         it should require the 'get:drinks-detail' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks 
-    is the list of drinks or appropriate status code indicating reason for failure
+    returns status code 200 and json {"success": True, "drinks": drinks} where
+    drinks is the list of drinks or appropriate status code indicating reason
+    for failure
 '''
 @app.route('/drinks-detail', methods=['GET'])
 @requires_auth('get:drinks-detail')
@@ -52,7 +56,8 @@ def drinks_detail(payload):
         drinks = Drink.query.all()
         drinks = [drink.long() for drink in drinks]
         return {'success': True, 'drinks': drinks}, 200
-    except:
+    except Exception as err:
+        print(sys.exc_info)
         abort(404)
 
 
@@ -62,9 +67,9 @@ def drinks_detail(payload):
         it should create a new row in the drinks table
         it should require the 'post:drinks' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an 
-    array containing only the newly created drink or appropriate status code indicating 
-    reason for failure
+    returns status code 200 and json {"success": True, "drinks": drink} where
+    drink is an array containing only the newly created drink or appropriate 
+    status code indicating reason for failure
 '''
 @app.route('/drinks', methods=['POST'])
 @requires_auth('post:drinks')
@@ -83,9 +88,9 @@ def create_drink(payload):
         it should update the corresponding row for <id>
         it should require the 'patch:drinks' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an 
-    array containing only the updated drink or appropriate status code indicating reason 
-    for failure
+    returns status code 200 and json {"success": True, "drinks": drink} where
+    drink an array containing only the updated drink or appropriate status code
+    indicating reason for failure
 '''
 @app.route('/drinks/<int:id>', methods=['PATCH'])
 @requires_auth('patch:drinks')
@@ -109,8 +114,9 @@ def update_drink(payload, id):
         it should respond with a 404 error if <id> is not found
         it should delete the corresponding row for <id>
         it should require the 'delete:drinks' permission
-    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
-        or appropriate status code indicating reason for failure
+    returns status code 200 and json {"success": True, "delete": id} where id is
+    the id of the deleted record or appropriate status code indicating reason for
+    failure
 '''
 @app.route('/drinks/<int:id>', methods=['DELETE'])
 @requires_auth('delete:drinks')
@@ -147,7 +153,7 @@ def unprocessable(error):
 
 '''
 @TODO implement error handler for 404
-    error handler should conform to general task above 
+    error handler should conform to general task above
 '''
 @app.errorhandler(404)
 def unfindable(error):
@@ -160,7 +166,7 @@ def unfindable(error):
 
 '''
 @TODO implement error handler for AuthError
-    error handler should conform to general task above 
+    error handler should conform to general task above
 '''
 @app.errorhandler(AuthError)
 def handle_auth_error(ex):
--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: "http://127.0.0.1:5000", // the running FLASK api server url
   auth0: {
-    url: "hcndcoffeeshop.eu.auth0.com", // the auth0 domain prefix
+    url: "hcndcoffeeshop.eu", // the auth0 domain prefix
     audience: "coffee", // the audience set for the auth0 app
     clientId: "04gGUgEcUNM8UcAhF1Z7xINMq8FRvECO", // the client id generated for the auth0 app
     callbackURL: "http://localhost:8100", // the base url of the running ionic application.
